cee2310c9868aca0c639f3706c440f5edb71002d
Humberto <delacruz101@gmail.com>
2022-03-23 05:51:06 -0400

    Initial Commit

---
+++ NightLive/NightLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightLiveApp.swift
+//  NightLive
+//
+//  Created by Sam Santos on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct NightLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
